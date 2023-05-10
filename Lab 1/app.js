6b6033af0b99f10d728dfdd95ab8530d40a24510
Victor <vcabredo@gmail.com>
2023-05-10 13:24:30 -0300

    update status code

--- Lab 1/app.js
+++ Lab 1/app.js
@@ -3,9 +3,9 @@ var os = require("os");
 var hostname = os.hostname();
 var app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello world from ' + hostname + '! Your app is up and running in a cluster!\n')
+app.get('/', function (req, res) {
+  res.status(404).json({ message: "This page is missing" })
 })
-app.listen(8080, function() {
+app.listen(8080, function () {
   console.log('Sample app is listening on port 8080.')
 })
